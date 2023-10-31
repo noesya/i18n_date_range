d1229fa27c3aac25f8d6161d6fccebc50ae59011
Arnaud Levy <contact@arnaudlevy.com>
2023-10-31 18:26:50 +0100

    fix

--- lib/i18n_date_range/i18n_date_range_helper.rb
+++ lib/i18n_date_range/i18n_date_range_helper.rb
@@ -6,8 +6,8 @@
 # Layout can be :one_line (default) or :two_lines, other might be necessary in the future
 module I18nDateRangeHelper
 
-  def date_range_i18n(from, to = nil, format: nil, layout: nil)
-    I18nDateRangeGenerator.new(*args).to_s
+  def date_range_i18n(from_date, to_date = nil, format: :short, layout: :one_line)
+    I18nDateRangeGenerator.new(from_date, to_date, format: format, layout: layout).to_s.html_safe
   end
 
 end
